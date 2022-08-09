8acffcfd6043db818c5bf186ec3953f24f974a7d
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2022-08-09 14:02:05 -0300

    bugfix(main.py)!: change data saving procedure and dataframe formatting

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 from brainflow.board_shim import BoardShim, BoardIds
 from brainflow.data_filter import DataFilter
 import pandas as pd
+import numpy as np
+from time import sleep
 
 import interface as interf
 from set_board import CYTON_BOARD_CONFIGURED, CYTON_ID
@@ -10,6 +12,8 @@ def main():
     interf.clear_screen()
     session_name = interf.get_session_name()
     session_number = interf.get_session_number()
+    volunteer_name = interf.get_volunteer_name()
+    volunteer_id = interf.get_volunteer_ID()
 
     BoardShim.enable_dev_board_logger()
     CYTON_BOARD_CONFIGURED.prepare_session()
@@ -17,17 +21,15 @@ def main():
     # TODO: check how the data is returned; official doc uses time.sleep
     if (CYTON_BOARD_CONFIGURED.is_prepared()):
         CYTON_BOARD_CONFIGURED.start_stream()
-        #Tempo de leitura (A definir)
+        sleep(10)
         data = CYTON_BOARD_CONFIGURED.get_board_data()
         CYTON_BOARD_CONFIGURED.stop_stream()
         CYTON_BOARD_CONFIGURED.release_session()
 
-    channels_names = CYTON_BOARD_CONFIGURED.get_eeg_names(CYTON_ID)
-    data_df = pd.DataFrame(data,columns=channels_names)
-    #Arquivo com colunas, mas sem indexação
-    data_df.to_csv(f'../data/session_{session_name}-{session_number}.csv', index=False)
-    #Arquivo com dados puramente
-    DataFilter.write_file(data, f'../data/session_{session_name}-{session_number}.csv', 'w')
+    columns_df = CYTON_BOARD_CONFIGURED.get_eeg_names(CYTON_ID)
+    columns_df.insert(0, 'Index')
+    data_df = pd.DataFrame(np.transpose(data)[: , :9], columns=columns_df)
+    DataFilter.write_file(data, f'data/session_{session_name}-{session_number}.csv', 'w')
 
 
 if __name__ == '__main__':
