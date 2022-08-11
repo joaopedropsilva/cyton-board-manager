ddbdb92c97bb04ebaa2556bf258e37078ff52b48
Edson Rodrigues <edson.er.rodrigues@gmail.com>
2022-08-11 17:58:23 -0300

    chore(get_data.py): Added prepare_session
    Nesse mesmo arquivo, não deixe de ver também a ideia de usar o pynput para controlar a cyton através de teclas e hotkeys

--- src/get_data.py
+++ src/get_data.py
@@ -1,16 +1,41 @@
 from brainflow.board_shim import BoardShim, NDArray
+from pynput.keyboard import Listener, Key, HotKey
 from nptyping import Float64
 from time import sleep
 
-from set_board import CYTON_BOARD_CONFIGURED
+from set_board import CYTON_BOARD_CONFIGURED as cyton
 
-def get_data(board: BoardShim, sampling_time: float = 10) -> NDArray[Float64]:
+def get_data(sampling_time: float = 10) -> NDArray[Float64]:
 
-    if board.is_prepared():
-        board.start_stream()
+    cyton.prepare_session()
+
+    if cyton.is_prepared():
+        cyton.start_stream()
         sleep(sampling_time)
-        data = board.get_board_data()
-        board.stop_stream()
-        board.release_session()
+        data = cyton.get_board_data()
+        cyton.stop_stream()
+        cyton.release_session()
 
         return data
+
+# Apenas uma ideia
+
+def on_press(key: Key):
+    
+    if cyton.is_prepared():
+
+        if key == 'p':
+            cyton.stop_stream()
+            print("fluxo de dados interrompido")
+        elif key == 's':
+            cyton.start_stream()
+            print("fluxo de dados retomado")
+        elif key == HotKey([Key.ctrl, Key.shift, 'c']):
+            cyton.stop_stream()
+            cyton.get_board_data()
+            print("Fluxo interrompido e dados descartado")
+
+def start_listener():
+    
+    listener = Listener(on_press=on_press)
+    listener.join()
