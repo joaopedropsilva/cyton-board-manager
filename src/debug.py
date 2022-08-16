86cf20275a6821324423b0b80acf186d3d7fef3f
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2022-08-16 15:04:18 -0300

    chore(debug.py): change writing in file procedure

--- src/debug.py
+++ src/debug.py
@@ -1,3 +1,4 @@
+from os import sep
 from brainflow.board_shim import BoardShim, BoardIds
 from brainflow.data_filter import DataFilter
 import pandas as pd
@@ -14,7 +15,7 @@ def debug():
     data = gd.get_data(sampling_time = 3)
 
     # File writing raw
-    DataFilter.write_file(data, f'data/debug-session_{session_number}.csv', 'w')
+    DataFilter.write_file(data, f'data_raw/debug-session_{session_number}.csv', 'w')
 
     # Column naming
     columns_df = CYTON_BOARD_CONFIGURED.get_eeg_names(CYTON_ID)
@@ -26,10 +27,9 @@ def debug():
     columns_df.append('Timestamp (Formatted)')
 
     # File writing with columns
-    data_raw = pd.read_csv(f'data/debug-session_{session_number}.csv')
-    print(columns_df)
-    input()
-    data_raw.to_csv(f'data_view/debug-session_{session_number}_formatted.csv', columns=columns_df)
+    data_raw = np.genfromtxt(f'data_raw/debug-session_{session_number}.csv', delimiter='\t')
+    data_df = pd.DataFrame(data_raw, columns=columns_df)
+    data_df.to_csv(f'data_formatted/debug-session_{session_number}_formatted.csv', sep='\t')
 
 if __name__ == '__main__':
     debug()
