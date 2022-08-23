818f8831cb48c116894ddfaafcd80c77b51361fe
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2022-08-23 14:44:34 -0300

    test(get_data): adding keyboard handling functions
    Not finished yet

--- src/get_data.py
+++ src/get_data.py
@@ -5,37 +5,29 @@ from time import sleep
 
 from set_board import CYTON_BOARD_CONFIGURED as cyton
 
+def on_press(key: Key):
+    if key.char == 'p':
+        cyton.stop_stream()
+        print("fluxo de dados interrompido")
+    elif key.char == 's':
+        cyton.start_stream()
+        print("fluxo de dados retomado")
+
+def start_listener():
+    listener = Listener(on_press = on_press)
+    listener.start()
+
 def get_data(sampling_time: float = 10) -> NDArray[Float64]:
 
     cyton.prepare_session()
 
     if cyton.is_prepared():
         cyton.start_stream()
+        start_listener()
         sleep(sampling_time)
         data = cyton.get_board_data()
+        # TODO: tratar exceção do cyton.stop_stream
         cyton.stop_stream()
         cyton.release_session()
 
         return data
-
-# Apenas uma ideia
-
-def on_press(key: Key):
-    
-    if cyton.is_prepared():
-
-        if key == 'p':
-            cyton.stop_stream()
-            print("fluxo de dados interrompido")
-        elif key == 's':
-            cyton.start_stream()
-            print("fluxo de dados retomado")
-        elif key == HotKey([Key.ctrl, Key.shift, 'c']):
-            cyton.stop_stream()
-            cyton.get_board_data()
-            print("Fluxo interrompido e dados descartado")
-
-def start_listener():
-    
-    listener = Listener(on_press=on_press)
-    listener.join()
