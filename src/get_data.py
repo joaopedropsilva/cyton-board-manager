1a8933270d2175117591af2e402343d1367a7cbe
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2022-09-13 13:32:40 -0300

    fix(get_data): correção da função on_press com relação aos tipos

--- src/get_data.py
+++ src/get_data.py
@@ -1,33 +1,42 @@
 from brainflow.board_shim import BoardShim, NDArray
-from pynput.keyboard import Listener, Key, HotKey
+from pynput.keyboard import Listener, Key, KeyCode
 from nptyping import Float64
 from time import sleep
 
 from set_board import CYTON_BOARD_CONFIGURED as cyton
 
-def on_press(key: Key):
-    if key.char == 'p':
-        cyton.stop_stream()
-        print("fluxo de dados interrompido")
-    elif key.char == 's':
-        cyton.start_stream()
-        print("fluxo de dados retomado")
+class LoopController():
+    def __init__(self) -> None:
+        self.state = True
 
-def start_listener():
-    listener = Listener(on_press = on_press)
-    listener.start()
+loop_controller = LoopController()
 
-def get_data(sampling_time: float = 10) -> NDArray[Float64]:
+def on_press(key) -> None:
+    if type(key) is KeyCode:
+        if key.char == 'p':
+            cyton.stop_stream()
+            print("Fluxo de dados interrompido")
+        elif key.char == 's':
+            cyton.start_stream()
+            print("Fluxo de dados retomado")
+    elif type(key) is Key:
+        if key == Key.esc:
+            cyton.stop_stream()
+            loop_controller.state = False
 
+listener = Listener(on_press = on_press)
+
+def get_data() -> NDArray[Float64]:
     cyton.prepare_session()
 
     if cyton.is_prepared():
+
         cyton.start_stream()
-        start_listener()
-        sleep(sampling_time)
+        listener.start()
+        while loop_controller.state:
+            sleep(1)
+            
         data = cyton.get_board_data()
-        # TODO: tratar exceção do cyton.stop_stream
-        cyton.stop_stream()
         cyton.release_session()
 
         return data
