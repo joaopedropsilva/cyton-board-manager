5e0ce5724bff1f6b5e945b564df188a571244459
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2022-10-18 15:00:02 -0300

    chore(filter): added bug report comment

--- src/filter.py
+++ src/filter.py
@@ -4,8 +4,10 @@ from brainflow.board_shim import NDArray
 from set_board import CYTON_SAMPLING_RATE
 from nptyping import Float64
 
-# Filters:
+## Bug:
+    # Precisa performar os métodos de filtragem de linha a linha (não dá pra filtrar o NDArray inteiro de uma vez só)
 
+# Filters:
 def envi_noise(data: NDArray[Float64]) -> None:
     DataFilter.remove_environmental_noise(data, CYTON_SAMPLING_RATE, NoiseTypes.FIFTY.value)
 
@@ -16,7 +18,6 @@ def bandstop(data: NDArray[Float64], start_freq: float = 50, stop_freq: float = 
     DataFilter.perform_bandstop(data, CYTON_SAMPLING_RATE, start_freq, stop_freq, order, FilterTypes.BUTTERWORTH.value, 0)
 
 # Apply filters
-
 def default_filtering(data: NDArray[Float64]) -> None:
     envi_noise(data)
     bandstop(data)
