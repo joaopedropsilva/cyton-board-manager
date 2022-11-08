5944e546bf8c2427b56e02575e494c35d9032e31
joaopedropsilva <83987930+joaopedropsilva@users.noreply.github.com>
2022-11-08 14:04:31 -0300

    fix(filter): fixed filtering methods arguments in functions

--- src/filter.py
+++ src/filter.py
@@ -1,23 +1,25 @@
 from brainflow.data_filter import DataFilter, FilterTypes, NoiseTypes
 from brainflow.board_shim import NDArray
 
-from set_board import CYTON_SAMPLING_RATE
+from set_board import CYTON_SAMPLING_RATE, CYTON_ID, CYTON_BOARD_CONFIGURED as cyton
 from nptyping import Float64
 
-## Bug:
-    # Precisa performar os métodos de filtragem de linha a linha (não dá pra filtrar o NDArray inteiro de uma vez só)
+# cyton.get_eeg_channels(CYTON_ID)
 
 # Filters:
 def envi_noise(data: NDArray[Float64]) -> None:
-    DataFilter.remove_environmental_noise(data, CYTON_SAMPLING_RATE, NoiseTypes.FIFTY.value)
+    for channel in cyton.get_eeg_channels(CYTON_ID):
+        DataFilter.remove_environmental_noise(data[channel], CYTON_SAMPLING_RATE, NoiseTypes.FIFTY.value)
 
 def bandpass(data: NDArray[Float64], start_freq: float, stop_freq: float, order: int = 4) -> None:
-    DataFilter.perform_bandpass(data, CYTON_SAMPLING_RATE, start_freq, stop_freq, order, FilterTypes.BESSEL.value, 0)
+    for channel in range(4):
+        DataFilter.perform_bandpass(data[channel], CYTON_SAMPLING_RATE, start_freq, stop_freq, order, FilterTypes.BESSEL.value, 0)
 
 def bandstop(data: NDArray[Float64], start_freq: float = 50, stop_freq: float = 70, order: int = 4) -> None:
-    DataFilter.perform_bandstop(data, CYTON_SAMPLING_RATE, start_freq, stop_freq, order, FilterTypes.BUTTERWORTH.value, 0)
+    for channel in range(4):
+        DataFilter.perform_bandstop(data[channel], CYTON_SAMPLING_RATE, start_freq, stop_freq, order, FilterTypes.BUTTERWORTH.value, 0)
 
 # Apply filters
 def default_filtering(data: NDArray[Float64]) -> None:
     envi_noise(data)
-    bandstop(data)
+    #bandstop(data)
